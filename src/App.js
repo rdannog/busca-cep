558511f8bdc175ceb899e0718e22cd4b66c560db
rdannog <rhayssadandara98@gmail.com>
2022-05-25 03:13:48 -0300

    styling page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styled, {createGlobalStyle} from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+}
+`
+const Container = styled.div`
+display:flex;
+flex-direction: column;
+justify-content:center;
+align-items:center;
+`
+const Header = styled.div`
+width:100%;
+background-color:#F5F3F0;
+display:flex;
+justify-content:center;
+align-items:center;
+`
 
 export default function App() {
   const [cep, setCep] = useState();
@@ -17,27 +39,40 @@ export default function App() {
       setCidade(response.data.localidade);
       setEstado(response.data.uf);
       setDdd(response.data.ddd);
+      setCep("")
     });
   };
 
-  return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <img
+  return (  
+    <Container>
+    <GlobalStyle/>
+    <title>Busca CEP</title>
+     <Header>
+     <img
         src="https://www.logomaker.com/api/main/images/1j+ojVVCOMkX9Wyrexe4hGfby7XT...Qw87X7bizxmfGwQoQJkmSErhPtt8vUtb0NZoBRejhUJd8U5jSBzUIAD3UQ6oXbQZs4AVn8="
         alt=""
       />
+     </Header>
+    <div>
+      <h1>Busca por Código de Endereçamento Postal Brasileiro</h1>
+      <p>Digite um CEP</p>
+    <form onSubmit={(e) => e.preventDefault()}>
       <input
         onChange={(e) => {
           setCep(e.target.value);
         }}
+        value={cep}
       />
       <button onClick={busca}>Buscar</button>
-
+    </form>
+      <div>
       <p>Rua: {rua}</p>
       <p>Bairro: {bairro}</p>
       <p>Cidade: {cidade}</p>
       <p>Estado: {estado}</p>
       <p>DDD: {ddd}</p>
-    </form>
+      </div>
+    </div>
+    </Container>
   );
 }
