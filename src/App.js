f7bb52d762684bea460072489b4948b246d98aaa
rdannog <rhayssadandara98@gmail.com>
2022-06-01 02:38:22 -0300

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import axios from 'axios';
 import styled, {createGlobalStyle} from "styled-components"
 import './style.css'
 
-const GlobalStyle = createGlobalStyle`
-*{
-  margin:0;
-  padding:0;
-  box-sizing:border-box;
-  
-}
-`
+// const GlobalStyle = createGlobalStyle`
+// *{
+//   padding: 0;
+//   margin: 0;
+//   box-sizing: border-box; 
+//   list-style:none;
+//   text-decoration: none;
+// }
+// `
 const Container = styled.div`
 display:flex;
 flex-direction: column;
@@ -30,6 +31,35 @@ background-color:#F5F3F0;
 const Logo = styled.img`
 max-height:90%;
 margin-left: 42%;
+`
+const Menu = styled.div`
+margin-bottom: 4vh;
+margin-left: 3vw;
+`
+const Nav = styled.nav`
+width:200px;
+position:absolute;
+left:-200px;
+transition: all 0.5s;
+ul{
+  position: absolute;
+  top:8vh;
+  width: 100%;
+}
+a{  
+  height:30px;
+  display: flex;
+  align-items: center;
+  text-align:center;
+  padding-left:20px;
+  color:white;
+  text-transform: capitalize;
+  background-color: #0C2A78;
+  &:hover{
+    background-color: #3f5faf;
+    color: gold;
+  }
+}
 `
 const Main = styled.div`
 display:flex;
@@ -99,24 +129,24 @@ export default function App() {
 
   return (  
     <>
-    <GlobalStyle/>
+    {/* <GlobalStyle/> */}
     
     <Container>
      <Header>
-     <div class="container-menu">
+     <Menu>
         <input type="checkbox" id="check"/>
         <label class="label" for="check">
-          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 172 172"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{mixBlendMode: "normal"}}><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#0C2A78"><path d="M14.33333,35.83333v14.33333h143.33333v-14.33333zM14.33333,78.83333v14.33333h143.33333v-14.33333zM14.33333,121.83333v14.33333h143.33333v-14.33333z"></path></g></g></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 172 172" style={{ cursor:"pointer"}}><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{mixBlendMode: "normal"}}><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#0C2A78"><path d="M14.33333,35.83333v14.33333h143.33333v-14.33333zM14.33333,78.83333v14.33333h143.33333v-14.33333zM14.33333,121.83333v14.33333h143.33333v-14.33333z"></path></g></g></svg>
        </label>
-        <nav class="nav-menu">
+        <Nav>
           <ul>
             <li><a href="#">link 1</a></li>
             <li><a href="#">link 2</a></li>
             <li><a href="#">link 3</a></li>
             <li><a href="#">link 4</a></li>
           </ul>
-        </nav>
-      </div>
+        </Nav>
+      </Menu>
      <Logo
         src="https://www.logomaker.com/api/main/images/1j+ojVVCOMkX9Wyrexe4hGfby7XT...Qw87X7bizxmfGwQoQJkmSErhPtt8vUtb0NZoBRejhUJd8U5jSBzUIAD3UQ6oXbQZs4AVn8="
         alt=""
